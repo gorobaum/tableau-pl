3c37724926c9590c4e0865b0b7c525128c7d2727
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-06-13 08:55:52 -0300

    tableau da fase 1

--- simplex.m
+++ simplex.m
@@ -1,39 +1,48 @@
 function [ind, x] = simplex(A,b,c,m,n,print)
-  [B, r] = passo1(A,b,c,m,n,print)
+  [ind, x] = preparesimplex(A,b,c,m,n,print)
 endfunction
 
-function [B, r] = passo1(A,b,c,m,n,print)
-  for i = 1:length (b)
-    if ( b(i) < 0)
-      b(i) = -1 * b(i)
+function [ind, x] = preparesimplex(A,b,c,m,n,print)
+  for i = 1:m
+    if ( b(i) < 0.0 )
+      auxb(i) = -b(i)
+      auxA(:,i) = -A(:,i)
+    else
+      auxb(i) = b(i)
+      auxA(:,i) = A(:,i)
     endif
   endfor
-  B = eye(m)
   for i = 1:n
-    x(i) = 0
+    indbase = 0
+    auxc(i) = 0.0
   endfor
-  [ind, x] = realsimplex(A,B,b,c,m,n,print)
+  for j = 1:m
+    indbase = 1
+    auxc(j) = 1.0
+  endfor
+
+    
+  [ind, x, B] = phase1simplex(auxA,auxb,auxc,indbase,m,n,print)
 endfunction
 
-function [ind, x] =  realsimplex(A,B,b,c,m,n,print)
-  binv = inv(B)
-  j = 0
-  
-  while ( c(j) > 0.0 )
-    j++
-  endwhile
-  if ( j == length(c) )
-    ind = 0
-    stop = true
-  endif
-  
-  u = binv*A(:,j)
-  i = 0
-  while ( u(i) < 0.0 )
-    i++
-  endwhile
-  if ( i == length(u) )
-    ind = -1
-    stop = true
-  endif
+function [ind, x, B] = phase1simplex(A,b,c,indbase,m,n,print)
+  I = eye(m)
+  auxA = [auxA,I]
+  auxA = [auxb',A]
+  auxc(1) = -(base(c,indbase))'*b
+  for i = 2:m+n
+    auxc = c(i) - auxc()
+  endfor
+  auxA = [auxc;auxA]
+
+endfunction
+
+function ret = base(vec, b)
+  j = 1
+  for i = 1:length(vec)
+    if ( i == b(j) )
+      ret(j) = vec(i)
+      j++
+    endif
+  endfor
 endfunction
