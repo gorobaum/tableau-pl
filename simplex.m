3d6ad01c34d26a3e9935992351075fefa8a01c56
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-06-15 18:01:24 -0300

    Sáporra tá funfando. EU ACHO

--- simplex.m
+++ simplex.m
@@ -25,29 +25,34 @@ function [ind, x] = preparesimplex(A,b,c,m,n,print)
   I = eye(m);
   auxA = [auxA,I];
   auxc(1) = -(base(cf1,indbase))'*auxb;
-  cf1'-(base(cf1,indbase))'*auxA;
   auxc = [auxc(1),cf1'-(base(cf1,indbase))'*auxA];
   auxA = [auxb,auxA];
   auxA = [auxc;auxA];
   
   disp("Fase 1")
-  [ind, x, B, indb] = runsimplex(auxA,auxb,auxc,indbase,m,m+n,print)
+  [ind, x, B, indb, m] = runsimplex(auxA,auxb,auxc,indbase,m,m+n,print)
+  B = B(:,1:n+1);
+  [B, indb, m] = removeslackformbase(B,m,n,indb)
   if (ind == -1 )
-    ind = 1
+    ind = 1;
   else
-    B = B(:,1:n+1)
     disp("Fase 2")
+    auxc(1) = -(base(c,indb))'*B(2:m+1,1)
+    auxc = [auxc(1),c'-(base(c,indb))'*B(2:m+1,2:n+1)]
+    B(1,:) = auxc
+    disp("Phase 2 hativaaa")
     [ind, x, B] = runsimplex(B,b,c,indb,m,n,print)
   endif
 endfunction
 
-function [ind, x, B, indb] = runsimplex(A,b,c,indbase,m,n,print)
+function [ind, x, B, indb, m] = runsimplex(A,b,c,indbase,inm,n,print)
   
   stop = false
   while (!stop)
     disp("Novo passo")
-    A
+    A;
     j = 2;
+    m = inm;
     while ( j <= length(A(1,:)) && A(1,j) >= 0.0 )
       j++;
     endwhile
@@ -68,7 +73,6 @@ function [ind, x, B, indb] = runsimplex(A,b,c,indbase,m,n,print)
         stop = true
         ind = -1
       else
-        l
         indbase(l-1) = j-1
         for i = 1:length(u)
           if ( i != l && u(i) != 0.0 )
@@ -79,26 +83,43 @@ function [ind, x, B, indb] = runsimplex(A,b,c,indbase,m,n,print)
       endif
     endif
   endwhile
-  B = A
-  x = A(2:m+1,1)
-  indb = indbase
+  disp("Final do simplex")
+  indb = indbase;
+  x = A(2:m+1,1);
+  B = A;
 endfunction
 
-function B = removeslackformbase(A,m,n,indbase)
+function [B, ind, m] = removeslackformbase(A,inm,n,indbase)
+  disp("Retirada da base")
   B = A
-  for i = 1:m
+  ind = indbase
+  length(indbase)
+  m = inm
+  for i = 2:m
     for j = m+1:m+n
-      if (indbase(i) == j )
+      if (indbase(i-1) == j )
+        k = 1;
+        while ( k <= length(A(i,:)) && A(i,k) == 0.0 )
+          k++;
+        endwhile
+        if ( k == length(A(i+1,:))+1 )
+          i
+          B = [B(1:i-1,:);B(i+1:m+1,:)];
+          ind = [ind(1:i-2),ind(i:m)];
+          m--;
+        else
+        endif
       endif
     endfor
   endfor
+  disp("Fim da retirada")
 endfunction
 
 function ret = base(vec, b)
   j = 1;
   ret = [];
   for i = 1:length(vec)
-    if ( i == b(j) )
+    if ( j <= length(b) && i == b(j) )
       ret = [ret;vec(i)];
       j++;
     endif
